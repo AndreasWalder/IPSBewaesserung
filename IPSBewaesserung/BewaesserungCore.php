5f119333818a96295313667400728cd1d0461487
Walder Andreas <andreas.walder@web.de>
2025-06-03 21:35:43 +0200

    Update BewaesserungCore.php

--- IPSBewaesserung/BewaesserungCore.php
+++ IPSBewaesserung/BewaesserungCore.php
@@ -9,6 +9,9 @@ class BewaesserungCore extends IPSModule
             $this->RegisterPropertyString("ZoneName$i", "Zone $i");
             $this->RegisterPropertyInteger("AktorID$i", 0);
         }
+        // Nebenstelle
+        $this->RegisterPropertyInteger("AktorID11", 0);
+
         $this->RegisterPropertyInteger("PumpeAktorID", 0);
 
         $this->RegisterVariableBoolean("GesamtAutomatik", "Automatik Gesamtsystem", "~Switch", 900);
@@ -20,14 +23,27 @@ class BewaesserungCore extends IPSModule
         $this->RegisterVariableBoolean("PumpeStatus", "Pumpe Status", "~Switch", 951);
         $this->RegisterVariableString("PumpeInfo", "Pumpe Info", "", 952);
 
-        // Prio-Startzeiten als Attribute
+        // Nebenstelle (11. Zone)
+        $nebenName = "Nebenstelle";
+        $this->RegisterVariableBoolean("Manuell11", "Manuell $nebenName", "~Switch", 1110);
+        $this->EnableAction("Manuell11");
+        $this->RegisterVariableBoolean("Automatik11", "Automatik $nebenName", "~Switch", 1111);
+        $this->EnableAction("Automatik11");
+        $this->RegisterVariableInteger("Dauer11", "Dauer $nebenName", "IPSBW.Duration", 1112);
+        $this->EnableAction("Dauer11");
+        $this->RegisterVariableInteger("Prio11", "Priorität $nebenName", "IPSBW.Prioritaet", 1113);
+        $this->EnableAction("Prio11");
+        $this->RegisterVariableBoolean("Status11", "Status $nebenName (EIN/AUS)", "~Switch", 1114);
+        $this->RegisterVariableString("Info11", "Info $nebenName", "", 1115);
+
+        // Prio-Startzeiten
         for ($p = 0; $p <= 99; $p++) {
             $this->RegisterAttributeInteger("StartPrio$p", 0);
         }
 
         $this->RegisterTimer("EvaluateTimer", 1000, 'IPS_RequestAction($_IPS["TARGET"], "Evaluate", 0);');
 
-        // Profile anlegen
+        // Profile
         if (!IPS_VariableProfileExists("IPSBW.Duration")) {
             IPS_CreateVariableProfile("IPSBW.Duration", 1);
             IPS_SetVariableProfileText("IPSBW.Duration", "", " s");
@@ -42,142 +58,118 @@ class BewaesserungCore extends IPSModule
     }
 
     public function ApplyChanges()
-{
-    parent::ApplyChanges();
-    $zoneCount = $this->ReadPropertyInteger("ZoneCount");
-    if ($zoneCount < 1) $zoneCount = 1;
-    if ($zoneCount > 10) $zoneCount = 10;
-
-    for ($i = 1; $i <= $zoneCount; $i++) {
-        $zoneName = $this->ReadPropertyString("ZoneName$i");
-
-        // Variablen registrieren (wie immer)
-        $this->RegisterVariableBoolean("Manuell$i", "Manuell $zoneName", "~Switch", 1000 + $i * 10);
-        $this->EnableAction("Manuell$i");
-        $this->RegisterVariableBoolean("Automatik$i", "Automatik $zoneName", "~Switch", 1001 + $i * 10);
-        $this->EnableAction("Automatik$i");
-        $this->RegisterVariableInteger("Dauer$i", "Dauer $zoneName", "IPSBW.Duration", 1002 + $i * 10);
-        $this->EnableAction("Dauer$i");
-        $this->RegisterVariableInteger("Prio$i", "Priorität $zoneName", "IPSBW.Prioritaet", 1003 + $i * 10);
-        $this->EnableAction("Prio$i");
-        $this->RegisterVariableBoolean("Status$i", "Status $zoneName (EIN/AUS)", "~Switch", 1004 + $i * 10);
-        $this->RegisterVariableString("Info$i", "Info $zoneName", "", 1005 + $i * 10);
-
-        // Variablen-Namen (Caption) immer updaten!
-        IPS_SetName($this->GetIDForIdent("Manuell$i"), "Manuell $zoneName");
-        IPS_SetName($this->GetIDForIdent("Automatik$i"), "Automatik $zoneName");
-        IPS_SetName($this->GetIDForIdent("Dauer$i"), "Dauer $zoneName");
-        IPS_SetName($this->GetIDForIdent("Prio$i"), "Priorität $zoneName");
-        IPS_SetName($this->GetIDForIdent("Status$i"), "Status $zoneName (EIN/AUS)");
-        IPS_SetName($this->GetIDForIdent("Info$i"), "Info $zoneName");
-
-        // Warnung bei fehlender AktorID
-        $aktorID = $this->ReadPropertyInteger("AktorID$i");
-        $infoID = $this->GetIDForIdent("Info$i");
-        if ($aktorID == 0) {
-            SetValueString($infoID, "Bitte KNX-Aktor für $zoneName auswählen!");
-        }
-    }
-
-    // Auch Pumpen-Variablen Caption updaten (falls gewünscht)
-    IPS_SetName($this->GetIDForIdent("PumpeManuell"), "Pumpe Manuell");
-    IPS_SetName($this->GetIDForIdent("PumpeStatus"), "Pumpe Status");
-    IPS_SetName($this->GetIDForIdent("PumpeInfo"), "Pumpe Info");
-
-    $this->SetTimerInterval("EvaluateTimer", 1000);
-    $this->ResetAllPrioStarts();
-}
-
-
-    public function RequestAction($Ident, $Value)
     {
-        if ($Ident == "Evaluate") {
-            $this->Evaluate();
-            return;
-        }
-        if ($Ident == "ResetAll") {
-            $this->ResetAllPrioStarts();
-            return;
-        }
-        if ($Ident == "GesamtAutomatik") {
-            if ($Value) {
-                $this->ResetAllPrioStarts();
-            }
-            SetValue($this->GetIDForIdent($Ident), $Value);
-            return;
-        }
-        if ($Ident == "PumpeManuell") {
-            SetValue($this->GetIDForIdent("PumpeManuell"), $Value);
-            return;
-        }
-        SetValue($this->GetIDForIdent($Ident), $Value);
-    }
-
-    public function Evaluate()
-    {
-        $now = time();
+        parent::ApplyChanges();
         $zoneCount = $this->ReadPropertyInteger("ZoneCount");
         if ($zoneCount < 1) $zoneCount = 1;
         if ($zoneCount > 10) $zoneCount = 10;
 
-        $gesamtAuto = GetValue($this->GetIDForIdent("GesamtAutomatik"));
-        $pumpeAktorID = $this->ReadPropertyInteger("PumpeAktorID");
-        $pumpeManuell = GetValue($this->GetIDForIdent("PumpeManuell"));
-        $pumpeStatusID = $this->GetIDForIdent("PumpeStatus");
-        $pumpeInfoID = $this->GetIDForIdent("PumpeInfo");
+        // Normale Zonen
+        for ($i = 1; $i <= $zoneCount; $i++) {
+            $zoneName = $this->ReadPropertyString("ZoneName$i");
 
-        $pumpeSollAn = $gesamtAuto || $pumpeManuell;
+            $this->RegisterVariableBoolean("Manuell$i", "Manuell $zoneName", "~Switch", 1000 + $i * 10);
+            $this->EnableAction("Manuell$i");
+            $this->RegisterVariableBoolean("Automatik$i", "Automatik $zoneName", "~Switch", 1001 + $i * 10);
+            $this->EnableAction("Automatik$i");
+            $this->RegisterVariableInteger("Dauer$i", "Dauer $zoneName", "IPSBW.Duration", 1002 + $i * 10);
+            $this->EnableAction("Dauer$i");
+            $this->RegisterVariableInteger("Prio$i", "Priorität $zoneName", "IPSBW.Prioritaet", 1003 + $i * 10);
+            $this->EnableAction("Prio$i");
+            $this->RegisterVariableBoolean("Status$i", "Status $zoneName (EIN/AUS)", "~Switch", 1004 + $i * 10);
+            $this->RegisterVariableString("Info$i", "Info $zoneName", "", 1005 + $i * 10);
 
-        if ($pumpeAktorID > 0 && @IPS_ObjectExists($pumpeAktorID)) {
-            $this->SafeRequestAction($pumpeAktorID, $pumpeSollAn, $pumpeStatusID, $pumpeInfoID, $pumpeSollAn ? "Pumpe EIN" : "Pumpe AUS");
-        } else {
-            SetValueBoolean($pumpeStatusID, false);
-            SetValueString($pumpeInfoID, "Keine Pumpe-AktorID");
+            IPS_SetName($this->GetIDForIdent("Manuell$i"), "Manuell $zoneName");
+            IPS_SetName($this->GetIDForIdent("Automatik$i"), "Automatik $zoneName");
+            IPS_SetName($this->GetIDForIdent("Dauer$i"), "Dauer $zoneName");
+            IPS_SetName($this->GetIDForIdent("Prio$i"), "Priorität $zoneName");
+            IPS_SetName($this->GetIDForIdent("Status$i"), "Status $zoneName (EIN/AUS)");
+            IPS_SetName($this->GetIDForIdent("Info$i"), "Info $zoneName");
+
+            $aktorID = $this->ReadPropertyInteger("AktorID$i");
+            $infoID = $this->GetIDForIdent("Info$i");
+            if ($aktorID == 0) {
+                SetValueString($infoID, "Bitte KNX-Aktor für $zoneName auswählen!");
+            }
         }
 
-        // Zonen-Logik (siehe vorherige Versionen, Beispielhaft für Automatik aus)
-        if ($gesamtAuto) {
-            for ($i = 1; $i <= $zoneCount; $i++) {
-                $zoneName = $this->ReadPropertyString("ZoneName$i");
-                $aktorID = $this->ReadPropertyInteger("AktorID$i");
-                $statusID = $this->GetIDForIdent("Status$i");
-                $infoID = $this->GetIDForIdent("Info$i");
-                if ($aktorID > 0 && @IPS_ObjectExists($aktorID)) {
-                    SetValueString($infoID, "Automatik aktiv, Manuell gesperrt");
+        // Nebenstelle (Zone 11, Name fest)
+        $nebenName = "Nebenstelle";
+        $this->RegisterVariableBoolean("Manuell11", "Manuell $nebenName", "~Switch", 1110);
+        $this->EnableAction("Manuell11");
+        $this->RegisterVariableBoolean("Automatik11", "Automatik $nebenName", "~Switch", 1111);
+        $this->EnableAction("Automatik11");
+        $this->RegisterVariableInteger("Dauer11", "Dauer $nebenName", "IPSBW.Duration", 1112);
+        $this->EnableAction("Dauer11");
+        $this->RegisterVariableInteger("Prio11", "Priorität $nebenName", "IPSBW.Prioritaet", 1113);
+        $this->EnableAction("Prio11");
+        $this->RegisterVariableBoolean("Status11", "Status $nebenName (EIN/AUS)", "~Switch", 1114);
+        $this->RegisterVariableString("Info11", "Info $nebenName", "", 1115);
+
+        IPS_SetName($this->GetIDForIdent("Manuell11"), "Manuell $nebenName");
+        IPS_SetName($this->GetIDForIdent("Automatik11"), "Automatik $nebenName");
+        IPS_SetName($this->GetIDForIdent("Dauer11"), "Dauer $nebenName");
+        IPS_SetName($this->GetIDForIdent("Prio11"), "Priorität $nebenName");
+        IPS_SetName($this->GetIDForIdent("Status11"), "Status $nebenName (EIN/AUS)");
+        IPS_SetName($this->GetIDForIdent("Info11"), "Info $nebenName");
+
+        $aktorID = $this->ReadPropertyInteger("AktorID11");
+        $infoID = $this->GetIDForIdent("Info11");
+        if ($aktorID == 0) {
+            SetValueString($infoID, "Bitte KNX-Aktor für $nebenName auswählen!");
+        }
+
+        IPS_SetName($this->GetIDForIdent("PumpeManuell"), "Pumpe Manuell");
+        IPS_SetName($this->GetIDForIdent("PumpeStatus"), "Pumpe Status");
+        IPS_SetName($this->GetIDForIdent("PumpeInfo"), "Pumpe Info");
+
+        $this->SetTimerInterval("EvaluateTimer", 1000);
+        $this->ResetAllPrioStarts();
+    }
+
+    // Die Methoden RequestAction(), Evaluate() usw. müssen ggf. ebenfalls auf "11" erweitert werden!
+    // Hier Beispiel für Evaluate(), manuell:
+    public function Evaluate()
+    {
+        $zoneCount = $this->ReadPropertyInteger("ZoneCount");
+        if ($zoneCount < 1) $zoneCount = 1;
+        if ($zoneCount > 10) $zoneCount = 10;
+
+        // Normale Zonen
+        for ($i = 1; $i <= $zoneCount; $i++) {
+            $manuell = GetValue($this->GetIDForIdent("Manuell$i"));
+            $aktorID = $this->ReadPropertyInteger("AktorID$i");
+            $statusID = $this->GetIDForIdent("Status$i");
+            $infoID = $this->GetIDForIdent("Info$i");
+            if ($aktorID > 0 && @IPS_ObjectExists($aktorID)) {
+                if ($manuell) {
+                    $this->SafeRequestAction($aktorID, true, $statusID, $infoID, "Manuell eingeschaltet");
                 } else {
-                    SetValueBoolean($statusID, false);
-                    SetValueString($infoID, "Keine AktorID");
+                    $this->SafeRequestAction($aktorID, false, $statusID, $infoID, "Manuell ausgeschaltet");
                 }
-            }
-            // Hier folgt die komplette Automatik-Logik...
-        } else {
-            // Nur wenn Automatik AUS: Manuell möglich!
-            for ($i = 1; $i <= $zoneCount; $i++) {
-                $zoneName = $this->ReadPropertyString("ZoneName$i");
-                $manuell = GetValue($this->GetIDForIdent("Manuell$i"));
-                $aktorID = $this->ReadPropertyInteger("AktorID$i");
-                $statusID = $this->GetIDForIdent("Status$i");
-                $infoID = $this->GetIDForIdent("Info$i");
-                if ($aktorID > 0 && @IPS_ObjectExists($aktorID)) {
-                    if ($manuell) {
-                        $this->SafeRequestAction($aktorID, true, $statusID, $infoID, "Manuell eingeschaltet");
-                    } else {
-                        $this->SafeRequestAction($aktorID, false, $statusID, $infoID, "Manuell ausgeschaltet");
-                    }
-                } else {
-                    SetValueBoolean($statusID, false);
-                    SetValueString($infoID, "Keine AktorID");
-                }
-            }
-            // Pumpe manuell nachziehen
-            if ($pumpeAktorID > 0 && @IPS_ObjectExists($pumpeAktorID)) {
-                $this->SafeRequestAction($pumpeAktorID, $pumpeManuell, $pumpeStatusID, $pumpeInfoID, $pumpeManuell ? "Pumpe EIN" : "Pumpe AUS");
             } else {
-                SetValueBoolean($pumpeStatusID, false);
-                SetValueString($pumpeInfoID, "Keine Pumpe-AktorID");
+                SetValueBoolean($statusID, false);
+                SetValueString($infoID, "Keine AktorID");
             }
+        }
+
+        // Nebenstelle (Zone 11)
+        $manuell = GetValue($this->GetIDForIdent("Manuell11"));
+        $aktorID = $this->ReadPropertyInteger("AktorID11");
+        $statusID = $this->GetIDForIdent("Status11");
+        $infoID = $this->GetIDForIdent("Info11");
+        if ($aktorID > 0 && @IPS_ObjectExists($aktorID)) {
+            if ($manuell) {
+                $this->SafeRequestAction($aktorID, true, $statusID, $infoID, "Manuell eingeschaltet");
+            } else {
+                $this->SafeRequestAction($aktorID, false, $statusID, $infoID, "Manuell ausgeschaltet");
+            }
+        } else {
+            SetValueBoolean($statusID, false);
+            SetValueString($infoID, "Keine AktorID");
         }
     }
+
 
     // Hilfsmethoden: protected, nicht private!
     protected function getPrioDauer($zoneArray)
