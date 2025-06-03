d8afc39f26af79a67350774501612e64228343a5
Walder Andreas <andreas.walder@web.de>
2025-06-03 21:08:26 +0200

    Update BewaesserungCore.php

--- IPSBewaesserung/BewaesserungCore.php
+++ IPSBewaesserung/BewaesserungCore.php
@@ -20,14 +20,14 @@ class BewaesserungCore extends IPSModule
         $this->RegisterVariableBoolean("PumpeStatus", "Pumpe Status", "~Switch", 951);
         $this->RegisterVariableString("PumpeInfo", "Pumpe Info", "", 952);
 
-        // Prio-Startzeiten
+        // Prio-Startzeiten als Attribute
         for ($p = 0; $p <= 99; $p++) {
             $this->RegisterAttributeInteger("StartPrio$p", 0);
         }
 
         $this->RegisterTimer("EvaluateTimer", 1000, 'IPS_RequestAction($_IPS["TARGET"], "Evaluate", 0);');
 
-        // Profile
+        // Profile anlegen
         if (!IPS_VariableProfileExists("IPSBW.Duration")) {
             IPS_CreateVariableProfile("IPSBW.Duration", 1);
             IPS_SetVariableProfileText("IPSBW.Duration", "", " s");
@@ -122,9 +122,96 @@ class BewaesserungCore extends IPSModule
             SetValueString($pumpeInfoID, "Keine Pumpe-AktorID");
         }
 
-        // (Zonen-Logik wäre hier, für Kürze ausgelassen.)
+        // Zonen-Logik (siehe vorherige Versionen, Beispielhaft für Automatik aus)
+        if ($gesamtAuto) {
+            for ($i = 1; $i <= $zoneCount; $i++) {
+                $zoneName = $this->ReadPropertyString("ZoneName$i");
+                $aktorID = $this->ReadPropertyInteger("AktorID$i");
+                $statusID = $this->GetIDForIdent("Status$i");
+                $infoID = $this->GetIDForIdent("Info$i");
+                if ($aktorID > 0 && @IPS_ObjectExists($aktorID)) {
+                    SetValueString($infoID, "Automatik aktiv, Manuell gesperrt");
+                } else {
+                    SetValueBoolean($statusID, false);
+                    SetValueString($infoID, "Keine AktorID");
+                }
+            }
+            // Hier folgt die komplette Automatik-Logik...
+        } else {
+            // Nur wenn Automatik AUS: Manuell möglich!
+            for ($i = 1; $i <= $zoneCount; $i++) {
+                $zoneName = $this->ReadPropertyString("ZoneName$i");
+                $manuell = GetValue($this->GetIDForIdent("Manuell$i"));
+                $aktorID = $this->ReadPropertyInteger("AktorID$i");
+                $statusID = $this->GetIDForIdent("Status$i");
+                $infoID = $this->GetIDForIdent("Info$i");
+                if ($aktorID > 0 && @IPS_ObjectExists($aktorID)) {
+                    if ($manuell) {
+                        $this->SafeRequestAction($aktorID, true, $statusID, $infoID, "Manuell eingeschaltet");
+                    } else {
+                        $this->SafeRequestAction($aktorID, false, $statusID, $infoID, "Manuell ausgeschaltet");
+                    }
+                } else {
+                    SetValueBoolean($statusID, false);
+                    SetValueString($infoID, "Keine AktorID");
+                }
+            }
+            // Pumpe manuell nachziehen
+            if ($pumpeAktorID > 0 && @IPS_ObjectExists($pumpeAktorID)) {
+                $this->SafeRequestAction($pumpeAktorID, $pumpeManuell, $pumpeStatusID, $pumpeInfoID, $pumpeManuell ? "Pumpe EIN" : "Pumpe AUS");
+            } else {
+                SetValueBoolean($pumpeStatusID, false);
+                SetValueString($pumpeInfoID, "Keine Pumpe-AktorID");
+            }
+        }
     }
 
-    // ... Restliche Methoden wie getPrioDauer(), ResetAllPrioStarts(), SafeRequestAction(), SafeSetValueBoolean() ...
+    // Hilfsmethoden: protected, nicht private!
+    protected function getPrioDauer($zoneArray)
+    {
+        $max = 0;
+        foreach ($zoneArray as $z) {
+            if ($z['dauer'] > $max) $max = $z['dauer'];
+        }
+        return $max;
+    }
+
+    protected function ResetAllPrioStarts()
+    {
+        for ($prio = 0; $prio <= 99; $prio++) {
+            $this->WriteAttributeInteger("StartPrio" . $prio, 0);
+        }
+    }
+
+    protected function SafeRequestAction($aktorID, $value, $statusID, $infoID, $okText = "")
+    {
+        if ($aktorID > 0 && @IPS_ObjectExists($aktorID)) {
+            try {
+                if (@RequestAction($aktorID, $value) !== false) {
+                    SetValueBoolean($statusID, $value);
+                    if ($okText != "") {
+                        SetValueString($infoID, $okText);
+                    }
+                } else {
+                    SetValueBoolean($statusID, false);
+                    SetValueString($infoID, "Fehler beim Schalten (RequestAction)!");
+                }
+            } catch (Exception $e) {
+                SetValueBoolean($statusID, false);
+                SetValueString($infoID, "Fehler beim Schalten: " . $e->getMessage());
+            }
+        } else {
+            SetValueBoolean($statusID, false);
+            SetValueString($infoID, "Aktor existiert nicht!");
+        }
+    }
+
+    protected function SafeSetValueBoolean($statusID, $value, $infoID, $text = "")
+    {
+        SetValueBoolean($statusID, $value);
+        if ($text != "") {
+            SetValueString($infoID, $text);
+        }
+    }
 }
 ?>
