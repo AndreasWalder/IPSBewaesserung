3f1005935a7323584ba1b477276c3b333693f94b
Walder Andreas <andreas.walder@web.de>
2025-08-06 01:00:37 +0200

    Update BewaesserungHelper.php

--- IPSBewaesserung/BewaesserungHelper.php
+++ IPSBewaesserung/BewaesserungHelper.php
@@ -35,7 +35,7 @@ trait BewaesserungHelper
     }
 
     // Setzt ALLE Prio-Startzeiten auf "offen" (0)
-    protected function ResetAllPrioStarts()
+    private function ResetAllPrioStarts()
     {
         for ($prio = 0; $prio <= 99; $prio++) {
             $this->WriteAttributeInteger("StartPrio" . $prio, 0);
