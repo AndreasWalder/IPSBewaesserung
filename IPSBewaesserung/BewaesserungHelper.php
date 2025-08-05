4a3ef999a3712b6a366b025c517a6e16b26204bd
Walder Andreas <andreas.walder@web.de>
2025-08-05 08:31:08 +0200

    Update BewaesserungHelper.php

--- IPSBewaesserung/BewaesserungHelper.php
+++ IPSBewaesserung/BewaesserungHelper.php
@@ -100,3 +100,49 @@ trait BewaesserungHelper
     }
 }
 ?>
+
+
+    public function RequestAction($Ident, $Value)
+    {
+        switch ($Ident) {
+            case "ManualNextStep":
+                if ($Value) {
+                    $this->ManualStepAdvance();
+                    $this->SetValue("ManualNextStep", false);
+                }
+                break;
+        }
+    }
+
+    private function ManualStepAdvance()
+    {
+        $zoneCount = $this->ReadPropertyInteger("ZoneCount");
+        $found = false;
+
+        for ($i = 1; $i <= $zoneCount; $i++) {
+            $statusID = $this->GetIDForIdent("Status$i");
+            if (!@IPS_VariableExists($statusID)) {
+                continue;
+            }
+
+            $status = GetValueBoolean($statusID);
+
+            if ($status && !$found) {
+                $aktorID = $this->ReadPropertyInteger("AktorID$i");
+                if ($aktorID > 0) {
+                    RequestAction($aktorID, false);
+                }
+                SetValueBoolean($statusID, false);
+                $found = true;
+            } elseif ($found && !$status) {
+                $aktorID = $this->ReadPropertyInteger("AktorID$i");
+                if ($aktorID > 0) {
+                    RequestAction($aktorID, true);
+                }
+                SetValueBoolean($statusID, true);
+                break;
+            }
+        }
+    }
+
+?>
